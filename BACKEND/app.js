dfac6397b2baf49e7dcaeef5af27a2d182d37e31
SudeepGowda18 <sudeepgowdam94@gmail.com>
2025-01-13 09:14:00 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,7 +3,6 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('HEllo world');
